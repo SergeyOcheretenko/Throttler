7d6b3d205138e924eb2c58e97c484147ded7c651
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2022-07-05 23:13:58 +0300

    Create stack handler condition

--- Throttler.js
+++ Throttler.js
@@ -4,7 +4,7 @@ class Throttler {
     constructor({ ms, requests }) {
         this.ms = ms;
         this.requests = requests;
-        this.stack = 1;
+        this.stack = 0;
         setInterval(() => {
             this.stack = 0;
         }, ms);
@@ -12,12 +12,17 @@ class Throttler {
 
     async acquire() {
         return new Promise((resolve, reject) => {
-            return (function waitForCondition() {
-                if (this.stack === 0) return resolve();
-                setTimeout(waitForCondition.bind(this), 30);
-            }).call(this);
+            const waitForCondition = () => {
+                if (this.stack < this.requests) {
+                    this.stack++;
+                    return resolve();
+                }
+                setTimeout(waitForCondition, 30);
+            };
+            return waitForCondition();
         });
     }
 }
 
 module.exports = Throttler;
+// export default Throttler;
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const Throttler = require('./Throttler');
 
-const throttler = new Throttler({ ms: 1000, requests: 10 });
+const throttler = new Throttler({ ms: 3000, requests: 6 });
 
 function request(...args) {
     console.log(args.join(' '));
@@ -13,4 +13,6 @@ async function throttledRequest(...args) {
     return request(...args);
 }
 
-throttledRequest('Program started');
+for (let i = 1; i <= 15; i++) {
+    throttledRequest(`${i} started`);
+}
