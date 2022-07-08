d97238d68459ef4836e303d522825bce45fce05d
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2022-07-08 19:59:25 +0300

    Update main test in index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
 'use strict';
 
-const Throttler = require('./Throttler');
+const Throttler = require('./throttler/Throttler');
+const PriorityThrottler = require('./throttler/PriorityThrottler');
 
-const throttler = new Throttler({ ms: 10, requests: 0 });
+const throttler = new PriorityThrottler({ ms: 100, requests: 10 });
 
-function request(...args) {
-    return args;
+async function generateRequests(throttler) {
+    const result = [];
+    const resultPromises = [];
+    
+    for (let i = 1; i <= 50; i++) {
+        const request = throttler.acquire(1)
+            .then(() => result.push(i));
+        resultPromises.push(request);
+
+        if (i % 3 === 0) {
+            const request = throttler.acquire(3)
+                .then(() => result.push(`Priority 3`));
+            resultPromises.push(request);
+        }
+
+        if (i % 5 === 0) {
+            const request = throttler.acquire(2)
+                .then(() => result.push(`Priority 2`));
+            resultPromises.push(request);
+        }
+    }
+    await Promise.all(resultPromises);
+    return result;
 }
 
-async function throttledRequest(...args) {
-    await throttler.acquire();
-    return request(...args);
+async function main() {
+    const data = await generateRequests(throttler);
+    console.log(data);
 }
 
-module.exports = throttledRequest;
+main(throttler);
